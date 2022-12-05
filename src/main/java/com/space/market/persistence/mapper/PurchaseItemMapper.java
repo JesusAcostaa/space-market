8a1e948ad6b9d25a0d2c4e2b3f3e833f8fa5e39c
JesusAcostaa <joacostaa@ufpso.edu.co>
2022-12-05 18:40:32 -0500

    Se agregan mapeadores para Compra y CompraProducto

---
+++ src/main/java/com/space/market/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,27 @@
+package com.space.market.persistence.mapper;
+
+import com.space.market.domain.PurchaseItem;
+import com.space.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+public interface PurchaseItemMapper {
+
+    @Mappings({
+            @Mapping(source = "id.idProducto" ,target = "productoId"),
+            @Mapping(source = "cantidad" ,target = "quantity"),
+            @Mapping(source = "estado" ,target = "active")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true),
+    })
+    ComprasProducto toComprasProducto(PurchaseItem purchaseItem);
+}
